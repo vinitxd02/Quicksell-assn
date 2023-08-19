43782dd19834c0e186c69bc0797d0a427e951de8
vinitxd02 <76154401+vinitxd02@users.noreply.github.com>
2023-08-19 07:07:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import KanbanBoard from './KanbanBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kanban Board</h1>
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
+
